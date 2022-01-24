cc21cc1101de08878d4190c34822447b6ad799cc
Alicja Szada <szadzia98@gmail.com>
2022-01-24 14:11:00 +0100

    add sql query

---
+++ przepisy.sql
@@ -0,0 +1,226 @@
+CREATE TABLE przepisy (
+    nazwa text,
+    opis text,
+    skladniki text,
+    id int 
+);
+INSERT INTO przepisy
+VALUES ('TIRAMISU Z DUSZONYMI JABŁKAMI I CIASTECZKAMI CANTUCCINI', 
+'SKŁADNIKI
+jabłka – 3 szt.
+cukier puder – 50 g (trochę zostawiamy do dekoracji)
+śliwki w syropie – 600 g
+laska wanilii – 1 szt.
+serek mascarpone – 200 g
+masło – 80 g
+mleko – 50 ml
+sok jabłkowy – 150 ml
+ciasteczka cantuccini – 100 g
+słupki migdałowe – 60 g
+skórka otarta z 1 pomarańczy
+cynamon – ½ łyżeczki
+rum – do smaku (opcjonalnie)', 
+'PRZYGOTUJ:
+tarkę o drobnych oczkach
+SPOSÓB PRZYGOTOWANIA:
+KROK 1: KROIMY JABŁKA
+Jabłka myjemy, kroimy w ósemki i usuwamy gniazda nasienne.
+KROK 2: PODGRZEWAMY CUKIER, MASŁO I JABŁKA
+Masło i cukier podgrzewamy na patelni o dużej średnicy do chwili, gdy cukier zacznie się lekko karmelizować. Dodajemy cząstki jabłek i cynamon. Dusimy przez ok. 3 minuty, a następnie dodajemy sok jabłkowy i gotujemy do miękkości.
+KROK 3: PRZYGOTOWUJEMY ŚLIWKI
+Śliwki odsączamy z syropu i kroimy drobno. Opcjonalnie skrapiamy niewielką ilością rumu.
+KROK 4: PRZYGOTOWUJEMY MASĘ MASCARPONE
+Mascarpone przekładamy do miski. Laskę wanilii przekrawamy wzdłuż i za pomocą ostrza noża wyciągamy ze środka ziarenka, które dodajemy do mascarpone. Dodajemy cukier puder, mleko i skórkę otartą z pomarańczy. Całość ucieramy na gładką masę.
+KROK 5: SKŁADAMY TIRAMISU
+Ciasteczka cantuccini łamiemy na kawałki. Duszone jabłka rozgniatamy widelcem i dodajemy słupki migdałowe. W szklaneczkach układamy na przemian warstwami duszone jabłka, śliwki, masę z serka mascarpone i ciasteczka cantuccini. Dekorujemy cukrem pudrem.', 
+'1');
+INSERT INTO przepisy
+VALUES ('TOSTY FRANCUSKIE Z LODAMI I SAŁATKĄ OWOCOWĄ', 
+'SKŁADNIKI
+winogrona – 150 g
+nektarynki – 2 szt.
+pomarańcze – 2 szt.
+pieczywo tostowe – 4 kromki
+jajka – 2 szt.
+mleko – 100 ml
+cukier – 2 łyżki
+masło – 2 łyżki
+lody śmietankowe – 500 ml
+ubita śmietanka – 150 g
+marmolada z owoców cytrusowych – 2 łyżki
+cukier puder – 1 łyżeczka
+pistacje – 20 g', 
+'PRZYGOTUJ
+rózgę kuchenną
+gałkownicę do lodów
+sitko
+KROK 1: PRZYGOTOWUJEMY SAŁATKĘ OWOCOWĄ
+Winogrona przekrawamy na pół, nektarynki kroimy na cząstki, a pomarańcze obieramy i filetujemy. Owoce przekładamy do miski i mieszamy.
+KROK 2: SMAŻYMY TOSTY FRANCUSKIE
+Jajka roztrzepujemy w miseczce za pomocą rózgi lub widelca. Dodajemy mleko, mieszamy. W tak przygotowanej miksturze obtaczamy kromki pieczywa tostowego.
+Tosty smażymy partiami na patelni na rozpuszczonym maśle. Gdy zarumienią się z jednej strony, odwracamy je na drugą.
+KROK 3: PRZYGOTOWUJEMY SOS CYTRUSOWY
+Marmoladę cytrusową zagotowujemy w rondelku z odrobiną wody. Odstawiamy do wystudzenia.
+KROK 4: PODAJEMY TOSTY FRANCUSKIE
+Gałki lodów serwujemy na tostach francuskich razem z sałatką owocową. W osobnej miseczce podajemy ubitą śmietanę. Tosty dekorujemy z wierzchu sosem cytrusowym i posiekanymi pistacjami. Całość oprószamy cukrem pudrem przesianym przez sitko.', 
+'2');
+INSERT INTO przepisy
+VALUES ('WEGAŃSKIE CIASTO À LA PANNA COTTA', 
+'SKŁADNIKI
+SPÓD
+płatki owsiane błyskawiczne - 2 szklanki
+wiórki kokosowe - ½ szklanki
+olej kokosowy - 2 łyżki
+suszone morele - 18 szt.
+sól - 1 szczypta
+WARSTWA PANNA COTTY
+mleczko kokosowe Real Thai - 500 ml
+syrop klonowy - 4 łyżki
+ekstrakt waniliowy - 1 łyżeczka
+agar-agar - 1½ łyżeczki
+MUS MALINOWY
+świeże maliny - 2 szklanki
+agar-agar - 1½ łyżeczki
+syrop klonowy - 2 łyżki
+DO DEKORACJI
+świeże czerwone porzeczki - 2 gałązki', 
+'PRZYGOTUJ:
+blender
+papier do pieczenia
+formę o średnicy 20 cm
+sitko o drobnych oczkach
+SPOSÓB PRZYGOTOWANIA:
+KROK 1: PRZYGOTOWUJEMY MASĘ NA SPÓD CIASTA
+Wszystkie składniki potrzebne do przygotowania spodu łączymy w naczyniu blendera i miksujemy bardzo drobno, aż dokładnie się połączą. W trakcie miksowania co jakiś czas mieszamy masę, a jej nadmiar zsuwamy ze ścianek naczynia tak, by mokre składniki dobrze połączyły się z suchymi.
+Dno formy wykładamy papierem do pieczenia. Gotową masę przekładamy do formy, równomiernie rozprowadzamy po jej dnie i dociskamy łyżką. Spód wstawiamy do lodówki na czas przygotowywania warstwy panna cotty.
+KROK 2: PRZYGOTOWUJEMY WARSTWĘ PANNA COTTY
+Mleczko kokosowe łączymy w rondelku z syropem klonowym, ekstraktem waniliowym i agarem. Zagotowujemy, często mieszając. Masę odstawiamy do przestudzenia, przelewamy na schłodzony spód, a następnie ponownie odstawiamy do wystudzenia na ok. 30 minut w temperaturze pokojowej.
+Formę z warstwą panna cotty wstawiamy do lodówki na co najmniej 2 godziny.
+KROK 3: PRZYGOTOWUJEMY MUS MALINOWY
+W osobnym rondelku łączymy maliny z syropem klonowym i agarem. Zagotowujemy i gotujemy, co jakiś czas mieszając, aż maliny się rozpadną.
+Mus malinowy przecedzamy przez sitko o drobnych oczkach. Przecedzone maliny odstawiamy do przestudzenia, a następnie przelewamy na schłodzoną warstwę panna cotty. Na wierzch wykładamy gałązki czerwonych porzeczek.
+KROK 4: SCHŁADZAMY CIASTO
+Formę wkładamy do lodówki na minimum 2 godziny. Ciasto panna cotta najlepiej jednak przygotować wieczorem i umieścić je w lodówce na całą noc.', 
+'3');
+INSERT INTO przepisy
+VALUES ('BATONY CYTRYNOWE NA ORKISZOWYM SPODZIE', 
+'SKŁADNIKI
+SPÓD
+olej kokosowy – ¼ szklanki
+miód – 2-3 łyżki
+jajko – 1 szt.
+mąka orkiszowa – 1 szklanka
+płatki owsiane – 2 łyżki
+sól – 1 szczypta
+WYPEŁNIENIE
+cytryna – 1 szt.
+sok z cytryny – ½ szklanki
+miód – ½ szklanki
+jajka – 4 szt.
+żółtko – 1 szt.
+mąka ziemniaczana – 1 łyżka', 
+'PRZYGOTUJ
+formę o wymiarach 20 x 20 cm
+papier do pieczenia
+piekarnik rozgrzany do temperatury 180°C 
+KROK 1: PRZYGOTOWUJEMY SPÓD
+Do miski wlewamy olej kokosowy. Dodajemy jajko i miód. Cały czas mieszając, dodajemy mąkę orkiszową, płatki owsiane i sól. Masę przekładamy do foremki wyłożonej papierem do pieczenia. Równomiernie rozprowadzamy. Wkładamy do piekarnika rozgrzanego do temp. 180°C i pieczemy ok. 10 minut.
+KROK 2: PRZYGOTOWUJEMY CYTRYNOWE WYPEŁNIENIE
+Do miski wlewamy sok cytrynowy i wrzucamy skórkę otartą z cytryny. Dodajemy miód, jajka i dodatkowe żółtko. Całość delikatnie mieszamy trzepaczką. Dodajemy mąkę ziemniaczaną i ponownie mieszamy. Masę wylewamy na podpieczony spód. Pieczemy kolejne 20-25 minut – masa musi się ściąć.
+KROK 3: DEKORUJEMY CUKREM PUDREM
+Wyciągamy ciasto z piekarnika i studzimy. Można je włożyć do lodówki do schłodzenia na ok. 4 godziny. Przed podaniem całość oprószamy przesianym przez sito cukrem pudrem. Kroimy w kostkę.', 
+'4');
+INSERT INTO przepisy
+VALUES ('BABECZKI JOGURTOWE Z OWOCAMI', 
+'SKŁADNIKI
+BABECZKI
+mąka pszenna – 200 g
+proszek do pieczenia – 1 czubata łyżeczka
+jajka – 3 szt.
+cukier – 120 g
+skórka otarta z cytryny – 1 łyżeczka
+masło – 80 g
+świeże owoce (np. maliny, borówki i jeżyny) – 100 g
+jogurt naturalny – 150 g
+sól – 2 szczypty
+POLEWA JOGURTOWA
+biała czekolada – 100 g
+jogurt naturalny – 100 ml
+DO DEKORACJI
+świeże owoce (maliny, borówki i jeżyny) – 100 g
+listki mięty – 12 szt.
+DODATKOWO
+masło do wysmarowania formy
+mąka do oprószenia formy', 
+'PRZYGOTUJ:
+trzepaczkę do jajek
+tarkę o małych oczkach
+worek do szprycowania
+formę do pieczenia babeczek
+łopatkę silikonową
+piekarnik rozgrzany do temperatury 170°C
+papier do pieczenia
+PRZYGOTUJ WCZEŚNIEJ:
+Przed przystąpieniem do przygotowywania babeczek rozpuść masło w rondelku i odstaw do lekkiego ostudzenia.
+SPOSÓB PRZYGOTOWANIA:
+KROK 1: ŁĄCZYMY SUCHE SKŁADNIKI CIASTA
+Wszystkie suche składniki oprócz cukru mieszamy w misce.
+KROK 2: ŁĄCZYMY JAJKA Z CUKREM I POZOSTAŁYMI SKŁADNIKAMI
+W osobnej misce łączymy jajka z cukrem. Mieszamy trzepaczką.
+Dodajemy połączone wcześniej suche składniki ciasta, skórkę otartą z cytryny, jogurt naturalny i rozpuszczone wcześniej, delikatnie wystudzone masło. Całość mieszamy.
+KROK 3: DODAJEMY OWOCE
+Do ciasta dodajemy jeżyny pokrojone w plasterki oraz pozostałe owoce. Ponownie mieszamy całość.
+KROK 4: PIECZEMY BABECZKI
+Formę do pieczenia babeczek smarujemy niewielką ilością masła i oprószamy mąką.
+Ciasto przekładamy do worka cukierniczego, końcówkę worka odcinamy. Szprycujemy ciasto do ¾ wysokości foremek.
+Babeczki pieczemy ok. 20 minut w piekarniku rozgrzanym do temperatury 170°C. Po upieczeniu odstawiamy na ok. 30 minut do wystudzenia.
+KROK 5: PRZYGOTOWUJEMY POLEWĘ JOGURTOWĄ
+Pokruszoną na kawałki czekoladę rozpuszczamy w rondelku i odstawiamy do lekkiego ostudzenia. Dodajemy jogurt naturalny i dokładnie mieszamy.
+KROK 6: DEKORUJEMY BABECZKI
+Z arkusza papieru do pieczenia przygotowujemy kornet. Do środka wlewamy polewę jogurtową. Zamykamy kornet, odcinamy końcówkę.
+Babeczki dekorujemy polewą jogurtową, świeżymi owocami i listkami mięty.', 
+'5');
+INSERT INTO przepisy
+VALUES ('BISZKOPT SZPINAKOWO-BAZYLIOWY Z MUSEM MALINOWO-JOGURTOWYM', 
+'SKŁADNIKI
+BISZKOPT SZPINAKOWO-BAZYLIOWY
+świeży szpinak – 60 g
+świeża bazylia – 40 g
+mąka pszenna – 180 g
+limonka – 1 szt.
+jajka, rozmiar L – 2 szt.
+cukier – 80 g
+masło – 40 g
+proszek do pieczenia – 1 łyżeczka
+oliwa z oliwek – 1 łyżka
+MUS MALINOWO-JOGURTOWY
+maliny – 150 g
+jogurt naturalny – 100 g
+cukier puder – 50 g
+śmietanka UHT 30% – 200 ml
+żelatyna – 8 g
+zimna woda – 50 ml
+SOS MALINOWY
+maliny – 100 g
+cukier puder – 40 g
+wódka – 30 ml
+DO DEKORACJI
+maliny
+listki bazylii', 
+'PRZYGOTUJ
+piekarnik rozgrzany do 170°C
+KROK 1: MIKSUJEMY SZPINAK Z BAZYLIĄ
+Miksujemy szpinak z bazylią
+Szpinak i bazylię myjemy, osuszamy i miksujemy. Ocieramy skórkę z całej limonki i wyciskamy z niej sok – miksujemy wraz ze szpinakiem i bazylią.
+KROK 2: PIECZEMY BISZKOPT
+Jajka ubijamy. Stopniowo dosypujemy cukier i ubijamy na puszystą masę. Miękkie masło dodajemy do masy jajecznej, cały czas miksując. Dolewamy 1 łyżkę oliwy z oliwek, dodajemy zmiksowany szpinak z bazylią – mieszamy. Do masy przesiewamy mąkę z proszkiem do pieczenia. Składniki łączymy, energicznie mieszając. Ciasto przekładamy na blachę piekarnikową wyłożoną papierem do pieczenia i równomiernie rozsmarowujemy. Wkładamy do piekarnika rozgrzanego do 170°C na około 14-15 minut.
+Biszkopt wyciągamy z piekarnika i przekładamy na zimną blachę. Pozostawiamy do ostygnięcia.
+KROK 3: PRZYGOTOWUJEMY MUS MALINOWY
+Przygotowujemy mus malinowy
+Żelatynę namaczamy w 50 ml zimnej wody i rozpuszczamy w mikrofalówce bądź kąpieli wodnej. Maliny miksujemy wraz z cukrem pudrem i jogurtem naturalnym, a następnie łączymy w dużej misce z żelatyną. Schłodzoną śmietankę ubijamy, a następnie stopniowo dodajemy do masy malinowo-jogurtowej, cały czas mieszając. Mus odstawiamy do lodówki do stężenia (około 45-50 minut).
+Maliny miksujemy z cukrem pudrem, a następnie przecieramy przez sito. Mus łączymy z alkoholem, mieszamy.
+KROK 4: WYCINAMY KÓŁKA Z BISZKOPTU
+Przygotowujemy mus malinowy
+Pierścieniem cukierniczym o średnicy około 6-7 cm wycinamy z biszkoptu koła. Mus malinowo-jogurtowy przekładamy do worka cukierniczego.
+Za pomocą pierścienia cukierniczego wykrawamy z biszkoptu kółka o średnicy około 6-7 cm. Mus malinowo-jogurtowy przekładamy do worka cukierniczego. Na brzegach biszkoptu szprycujemy mus. Na środek nakładamy sos malinowy, który przykrywamy musem. Nakładamy kolejny biszkopt. Czynność powtarzamy. Na wierzchnim biszkopcie szprycujemy mus, wykładamy na środek sos i układamy świeże maliny. Przyozdabiamy listkami bazylii.', 
+'6');
